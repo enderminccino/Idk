99ca42dc00d3d8817b38ae204bbb1158de8797e8
enderminccino <70907346+enderminccino@users.noreply.github.com>
2025-06-09 15:48:51 -0700

    Update main.c

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int main() {
     int guessThisNumber = rand()%40;
     int playerGuessedThis = -1;
     char inputString[100];
-    printf("I'm thinking of a ######\ntags\n");
+    printf("I'm thinking of a number.\n");
         while (guessThisNumber != playerGuessedThis){
-            printf("Guess the ###### please: ");
+            printf("Guess the number please: ");
             gets(inputString);
              playerGuessedThis = atoi(inputString);
             if(playerGuessedThis > guessThisNumber){printf("Sorry, too high.\n");}
